5f414a89001ec9871ebed2b57200e14681c1f225
rajkumar23456 <23kittu23@gmail.com>
2021-10-02 23:29:23 +0530

    day12

---
+++ src/Credentials.js
@@ -0,0 +1,8 @@
+const Credentials = () => {
+  return {
+    ClientId: 'fc1d78be62514b6daf53db6552e0ddcd',
+    ClientSecret: 'de2156b4a2004228bd6123d4cfb6c4da',
+  };
+};
+
+export { Credentials };
